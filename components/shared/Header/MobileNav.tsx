37109c0523115bc231cae889d47e9b457f449dd9
faezehdev <faezeh0078@gmail.com>
2025-08-14 19:22:16 +0330

    update

--- components/shared/Header/MobileNav.tsx
+++ components/shared/Header/MobileNav.tsx
@@ -1,17 +1,17 @@
-import { useState } from 'react';
-import { slide as Menu } from 'react-burger-menu';
-import Nav from './Nav';
-import ToggleLangs from './ToggleLangs';
+// import { useState } from 'react';
+// import { slide as Menu } from 'react-burger-menu';
+// import Nav from './Nav';
+// import ToggleLangs from './ToggleLangs';
 const MobileNav = () => {
-    const open = false
+    // const open = false
 
     return (
         <>
             <div className="fixed left-0 top-0 bottom-0 right-0 z-30 w-screen h-[100px]">
-                <Menu isOpen={open} width={'100vw'} >
+                {/* <Menu isOpen={open} width={'100vw'} >
                     <ToggleLangs />
                     <Nav />
-                </Menu>
+                </Menu> */}
             </div>
 
         </>

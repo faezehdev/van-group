b8861a67753558c48f1f942ff2123ba1423283db
faezehdev <faezeh0078@gmail.com>
2025-08-15 16:49:12 +0330

    change

--- components/category/Category.tsx
+++ components/category/Category.tsx
@@ -12,7 +12,7 @@ const Category: FC<CategoryPropType> = ({ category }) => {
                        overflow-hidden group/cat w-full flex justify-center items-center py-3 bg-black1 rounded-2xl">
                 <div className="category-overlay releative duration-[1s] opacity-[0] items-center rounded-2xl
                          w-full h-full absolute top-0 group-hover/cat:opacity-[1] z-[1] bg-graylight flex justify-center ">
-                    <p className="text-2xl mr-auto text-black font-normal ">
+                    <p className="text-2xl mr-[34%] text-black font-normal ">
                         {category}
                     </p>
                     <span className="icon  left-0 ml-5 mr-auto translate-x-[100%] opacity-0 group-hover/cat:opacity-[1]
